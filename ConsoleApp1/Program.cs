dcd28a20489bf301dd80804c062251edd2eff15c
Pitzer34 <peter00880@gmail.com>
2022-10-24 19:26:51 +0800

    加入專案檔案。

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1+ 2 + 3 + 4 + .... + N <105,請問 N 是多少 ?
+
+            int number = 1;
+            int sum = 105;
+            while (sum >0)
+            {
+                number += 1;
+                sum -= number;
+            	
+            }
+            Console.WriteLine(number-1);
+        }
+        
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //使用 while , 在畫面顯示所有可以整除 300 的整數
+            //例如: 1, 2, 3, 4, 5, 6, 10, ...., 300
+
+            int number = 300;
+            int i = 1;
+            
+            while (i <= 300)
+            {
+                if (number % i == 0)
+                {
+                    Console.WriteLine(i);
+                }
+                i += 1;
+            }
+        }
+    }
+}
